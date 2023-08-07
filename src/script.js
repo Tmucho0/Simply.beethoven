ef645fa2bce9e483baf49d35e4b132ed15ea637d
juliussn <cabrera1308julian@gmail.com>
2023-08-07 12:11:17 -0300

    implementacion de notas individualmente

--- src/script.js
+++ src/script.js
@@ -1,10 +1,66 @@
 import "https://unpkg.com/tone@14.7.77/build/Tone.js";
-
-const btn = document.getElementById("Holitas");
-btn.addEventListener("click", async () => {
+"C4", // Do
+"D4", // Re
+"E4", // Mi
+"F4", // Fa
+"G4", // Sol
+"A4", // La
+"B4" // Si
+const Doblanca = document.getElementById("DoB");
+Doblanca.addEventListener("click", async () => {
     await Tone.start();
     const synth = new Tone.Synth().toDestination();
     synth.triggerAttackRelease("C4", "8n");    
+});
+
+const Reblanca = document.getElementById("ReB");
+Reblanca.addEventListener("click", async () => {
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("D4", "8n");   
+});
+
+const Miblanca = document.getElementById("MiB");
+Miblanca.addEventListener("click", async () => {
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("E4", "8n");
+});
+
+const Fablanca = document.getElementById("FaB");
+Fablanca.addEventListener("click", async () => {
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("F4", "8n");
+});
+
+
+const Solblanca = document.getElementById("SolB");
+Solblanca.addEventListener("click", async () => {
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("G4", "8n");
+});
+
+const Lablanca = document.getElementById("LaB");
+Lablanca.addEventListener("click", async () => {
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("A4", "8n");
+});
+
+const Siblanca = document.getElementById("SiB");
+Siblanca.addEventListener("click", async () => {
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("B4", "8n");
+});
+
+const Doblanca1 = document.getElementById("DoBM");
+Doblanca1.addEventListener("click", async () => {
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("C4", "8n");
 });
 
 // script.js
