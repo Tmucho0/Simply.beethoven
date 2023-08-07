d0f0615673a22e061f3424d2cb03ff90a3dae3a7
juliussn <cabrera1308julian@gmail.com>
2023-08-07 12:57:40 -0300

    comienzos de escala y acordes

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let Synth = import("https://unpkg.com/tone@14.7.77/build/eTone.js");
 const synth = new Tone.Synth().toDestination();
 const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5", "B5"]
 const notesnegras = ["C#5", "D#5", "F#5", "G#5", "A#5", "B#5", "C#6", "D#6", "F#6", "G#6", "A#6", "B#6"]
@@ -19,16 +20,51 @@ for (let i = 0; i < Negras.length; i++) {
 
 const playButtom = document.getElementById("BotonDePlay");
 playButtom.addEventListener("click", async () => {
-    synth.frequency.value = "D#4";
-    const armonizacion = [0, 4, 7, 12, 16, 19, 24];
-    for (const interval of armonizacion) {
-        const notaArmonizada = synth.frequency.value + interval;
-        synth.triggerAttackRelease(notaArmonizada, "4n");
-        await new Promise(resolve => setTimeout(resolve, 500)); 
-    }
+    const RootNote = document.getElementById("Root-note");
+    const EscalaMAYME = document.getElementById("Escalas");
+    const Escala = EscalaMAYME.value;
+    synth.frequency.value = RootNote.value;
 
-    console.log("holitas");
+    if (Escala === "Escala-Mayor") {
+        const armonizacion = [0, 2, 4, 5, 7, 9, 11];
+        for (const i of armonizacion) {
+            const notaArmonizada = synth.frequency.value * Math.pow(2, i / 8);
+            synth.triggerAttackRelease(notaArmonizada, "8n");
+            await new Promise(resolve => setTimeout(resolve, 500));
+
+        }
+    }
+    else if(Escala === "Escala-Menor") {
+        const armonizacion = [0, 2, 3, 5, 7, 8, 10];
+        for (const i of armonizacion) {
+            const notaArmonizada = synth.frequency.value * Math.pow(2, i / 8);
+            synth.triggerAttackRelease(notaArmonizada, "8n");
+            await new Promise(resolve => setTimeout(resolve, 500));
+
+        }
+    }
 });
+const playButtom2 = document.getElementById("BotonDePlay2");
+playButtom2.addEventListener("click", async () => {
+    const escalaSelect = document.getElementById("Escalas2").value;
+    if (escalaSelect === "Escala-Mayor2") {
+        const armonizacion = [0, 4, 7, 12];
+        for (const i of armonizacion) {
+            const notaArmonizada = synth.frequency.value * Math.pow(2, i / 12);
+            synth.triggerAttackRelease(notaArmonizada, "8n");
+            await new Promise(resolve => setTimeout(resolve, 500));
+        }
+    } else if (escalaSelect === "Escala-Menor2") {
+        const armonizacion = [0, 3, 7, 12];
+        for (const i of armonizacion) {
+            const notaArmonizada = synth.frequency.value * Math.pow(2, i / 12);
+            synth.triggerAttackRelease(notaArmonizada, "8n");
+            await new Promise(resolve => setTimeout(resolve, 500));
+        }
+    }
+});
+
+
 
 
 //Actividad a hacer:Representar la formula de la escala mayor en codigo, lo mismo con la escala menor.... Vamo que se puede kbaiero
