313c88863192587f0685c11ed3e9ce0aef8ddf45
juliussn <cabrera1308julian@gmail.com>
2023-08-07 11:53:48 -0300

    intentamos importar tone

--- src/script.js
+++ src/script.js
@@ -1,8 +1,14 @@
-// Esto sirve para implementar select2 al codigo.
+import "https://unpkg.com/tone@14.7.77/build/Tone.js";
+
+const btn = document.getElementById("Holitas");
+btn.addEventListener("click", async () => {
+    await Tone.start();
+    const synth = new Tone.Synth().toDestination();
+    synth.triggerAttackRelease("C4", "8n");    
+});
+
+// script.js
 
 $(document).ready(function () {
     $('.js-example-basic-single').select2();
 });
-
-const sintetizador = new Tone.Synth().toDestination();
-
