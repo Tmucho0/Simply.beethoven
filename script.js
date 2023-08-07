38c4f33f4be95bdd0c8f1150f8c3b199fa0a4086
juliussn <cabrera1308julian@gmail.com>
2023-08-07 12:39:32 -0300

    reduccion de notas blancas y negras

--- script.js
+++ script.js
@@ -1,183 +1,66 @@
 let Synth = import("https://unpkg.com/tone@14.7.77/build/eTone.js");
 const synth = new Tone.Synth().toDestination();
+const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5", "B5"]
+const notesnegras = ["C#5", "D#5", "F#5", "G#5", "A#5", "B#5", "C#6", "D#6", "F#6", "G#6", "A#6", "B#6"]
+const Escalas = document.getElementsByClassName("Escala")
+const Blancas = document.getElementsByClassName("white-key");
+const Negras = document.getElementsByClassName("black-key");
 
-//blancas menores y mayores
-const Doblanca = document.getElementById("DoB");
-Doblanca.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("C4", "4n");
-});
+for (let i = 0; i < Blancas.length; i++) {
+    Blancas[i].addEventListener("click", function() {
+        synth.triggerAttackRelease(notes[i], "4n");
+    });
+  }
 
-const Reblanca = document.getElementById("ReB");
-Reblanca.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("D4", "8n");   
-});
+for (let i = 0; i < Negras.length; i++) {
+    Negras[i].addEventListener("click", function() {
+        synth.triggerAttackRelease(notesnegras[i], "4n");
+    });
+}
 
-const Miblanca = document.getElementById("MiB");
-Miblanca.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("E4", "8n");
-});
-
-const Fablanca = document.getElementById("FaB");
-Fablanca.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("F4", "8n");
-});
-
-
-const Solblanca = document.getElementById("SolB");
-Solblanca.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("G4", "8n");
-});
-
-const Lablanca = document.getElementById("LaB");
-Lablanca.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("A4", "8n");
-});
-
-const Siblanca = document.getElementById("SiB");
-Siblanca.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("B4", "8n");
-});
-
-const Doblanca1 = document.getElementById("DoBM");
-Doblanca1.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("C5", "8n");
-});
-
-const Reblanca1 = document.getElementById("ReBM");
-Reblanca1.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("D5", "8n");
-});
-
-const Miblanca1 = document.getElementById("MiBM");
-Miblanca1.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("E5", "8n");
-});
-
-const Fablanca1 = document.getElementById("FaBM");
-Fablanca1.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("F5", "8n");
-});
-
-const Solblanca1 = document.getElementById("SolBM");
-Solblanca1.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("G5", "8n");
-});
-
-const Lablanca1 = document.getElementById("LaBM");
-Lablanca1.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("A5", "8n");
-});
-
-const Siblanca1 = document.getElementById("SiBM");
-Siblanca1.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("B5", "8n");
-});
-
-//negras mayores y menores
-const DoNegra = document.getElementById("DoNegra");
-DoNegra.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("C#4 ", "8n");
-});
-
-const ReNegra = document.getElementById("ReNegra");
-ReNegra.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("D#4", "8n"); 
-});
-
-const FaNegra = document.getElementById("FaNegra");
-FaNegra.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("F#4", "8n"); 
-});
-
-const SolNegra = document.getElementById("SolNegra");
-SolNegra.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("G#4", "8n"); 
-});
-
-const LaNegra = document.getElementById("LaNegra");
-LaNegra.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("A#4", "8n"); 
-});
-
-const DoNegraM = document.getElementById("DoNegraM");
-DoNegraM.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("B#5 ", "8n");
-});
-
-const ReNegraM = document.getElementById("ReNegraM");
-ReNegraM.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("D#5", "8n"); 
-});
-
-const FaNegraM = document.getElementById("FaNegraM");
-FaNegraM.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("F#5", "8n"); 
-});
-
-const SolNegraM = document.getElementById("SolNegraM");
-SolNegraM.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("G#5", "8n"); 
-});
-
-const LaNegraM = document.getElementById("LaNegraM");
-LaNegraM.addEventListener("click", async () => {
-    await Tone.start();
-    synth.triggerAttackRelease("A#5", "8n");
-});
-
+//Actividad a hacer:Representar la formula de la escala mayor en codigo, lo mismo con la escala menor.... Vamo que se puede kbaiero
 
 const playButtom = document.getElementById("playButtom");
+playButtom.addEventListener("click", async() => {
+    if (Escalas[0]) {
+        for (let i = 0; i < Blancas.length; i++) {
+            await Tone.start()
+            synth.triggerAttackRelease(notes[i], "8n");
+        }
+    } else{
+        for (let i = 0; i < Blancas.length - 1; i--) {
+            synth.triggerAttackRelease(notes[i], "8n");
+        }
+    }
+})
+
 playButtom.addEventListener("click", async () => {
     await Tone.start();
     synth.triggerAttackRelease("C4", "8n");
-    synth.triggerAttackRelease("C#4 ", "8n");
-    synth.triggerAttackRelease("D4", "8n", +0.5);
-    synth.triggerAttackRelease("D#4", "8n", +7.5);
-    synth.triggerAttackRelease("E4", "8n", +1);
-    synth.triggerAttackRelease("F4", "8n", +1.5);
-    synth.triggerAttackRelease("F#4", "8n", +8);
-    synth.triggerAttackRelease("G4", "8n", +2);
-    synth.triggerAttackRelease("G#4", "8n", +8.5);
-    synth.triggerAttackRelease("A4", "8n", +2,5);
-    synth.triggerAttackRelease("A#4", "8n", +9);
-    synth.triggerAttackRelease("B4", "8n", +3);
-    synth.triggerAttackRelease("C5", "8n", +3.5);
-    synth.triggerAttackRelease("C#5", "8n");
-    synth.triggerAttackRelease("D5", "8n", +4);
-    synth.triggerAttackRelease("D#5", "8n", +7.5);
-    synth.triggerAttackRelease("E5", "8n", +4.5);
-    synth.triggerAttackRelease("F5", "8n", +5);
-    synth.triggerAttackRelease("F#5", "8n", +8);
-    synth.triggerAttackRelease("G5", "8n", +5.5);
-    synth.triggerAttackRelease("G#5", "8n", +8.5);
-    synth.triggerAttackRelease("A5", "8n", +6);
-    synth.triggerAttackRelease("A#5", "8n", +6);
-    synth.triggerAttackRelease("B5", "8n", +6.5);
+    //synth.triggerAttackRelease("C#4 ", "8n", + 0.5);
+    synth.triggerAttackRelease("D4", "8n", + 1);
+    //synth.triggerAttackRelease("D#4", "8n", + 1.5);
+    synth.triggerAttackRelease("E4", "8n", + 1.5);
+    synth.triggerAttackRelease("F4", "8n", +2);
+    //synth.triggerAttackRelease("F#4", "8n", + 3);
+    synth.triggerAttackRelease("G4", "8n", + 2.5);
+    //synth.triggerAttackRelease("G#4", "8n", + 4);
+    synth.triggerAttackRelease("A4", "8n", + 3);
+    //synth.triggerAttackRelease("A#4", "8n", + 5);
+    synth.triggerAttackRelease("B4", "8n", + 3.5);
+    synth.triggerAttackRelease("C5", "8n", + 4);
+    //synth.triggerAttackRelease("C#5", "8n", + 6.5);
+    synth.triggerAttackRelease("D5", "8n", + 4.5);
+    //synth.triggerAttackRelease("D#5", "8n", + 7.5);
+    synth.triggerAttackRelease("E5", "8n", + 5);
+    synth.triggerAttackRelease("F5", "8n", + 5.5);
+    //synth.triggerAttackRelease("F#5", "8n", + 9);
+    synth.triggerAttackRelease("G5", "8n", + 6);
+    //synth.triggerAttackRelease("G#5", "8n", + 10);
+    synth.triggerAttackRelease("A5", "8n", + 6.5);
+    //synth.triggerAttackRelease("A#5", "8n", + 11);
+    synth.triggerAttackRelease("B5", "8n", + 7);
 });
-
 
 $(document).ready(function () {
     $('.js-example-basic-single').select2();
